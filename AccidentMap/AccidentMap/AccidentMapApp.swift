31002c6eb4f728f1686dedb7948ee6edaf75b695
ezzkimm <dmswjd1515@gmail.com>
2025-05-21 14:39:11 +0900

    project init

---
+++ AccidentMap/AccidentMap/AccidentMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccidentMapApp.swift
+//  AccidentMap
+//
+//  Created by 김은정 on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct AccidentMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccidentMap/AccidentMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AccidentMap
+//
+//  Created by 김은정 on 5/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
