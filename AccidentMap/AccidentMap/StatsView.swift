6364b37a449d5468c315a524285546a1c7c2d2c3
ezzkimm <dmswjd1515@gmail.com>
2025-05-27 16:07:16 +0900

    View file

---
+++ AccidentMap/AccidentMap/StatsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatsView.swift
+//  AccidentMap
+//
+//  Created by 김은정 on 5/27/25.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatsView()
+}
